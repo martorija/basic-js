ddf36ac39c03796c1f65e718230f02917f5a402d
martorija <martorija@mail.ru>
2022-10-17 14:54:10 +0300

    update count-cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,8 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+    let cat=0;
+  for(let i=0; i < backyard.length; i++){
+      if(backyard[i] == '^^'){
+        cat++;
+      }
+      // else{
+      //   return 0;
+      // }
+  }
+  console.log(countCats);
 }
 
 module.exports = {
